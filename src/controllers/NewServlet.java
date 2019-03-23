d1c8bd1678faa06ae895470c9740709198e8a8fe
yuri.yamazaki <u101663g@yahoo.co.jp>
2019-03-23 15:17:36 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
+
 
 /**
  * Servlet implementation class NewServlet
@@ -32,25 +31,13 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    em.getTransaction().begin();
 
-	    Task m = new Task();
+	    request.setAttribute("_token", request.getSession().getId());
 
-	    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
+	    request.setAttribute("task", new Task());
 
-        String content = "content";
-        m.setContent(content);
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+	    rd.forward(request, response);
 
-	    em.persist(m);
-        em.getTransaction().commit();
-
-		// TODO Auto-generated method stub
-		response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-		em.close();
 	}
-
 }
